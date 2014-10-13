2a60ac2e39e895c0d43b529e1e6375f6c8d58bde
Aaron Curtis <foobarbecue@gmail.com>
2014-10-13 15:18:39 -0600

    Now decodes incoming Int32Array data from the tracebuf.

--- client/seiscream.js
+++ client/seiscream.js
@@ -1,13 +1,33 @@
 if (Meteor.isClient) {
+  // TODO rename traces collection to messages because we can deal with other types 
+  // of messages too, and trace should be reserved for referring to the data in a
+  // tracebuf message.
+  Messages = new Meteor.Collection("ew_traces");
+  Meteor.subscribe('messages')
   // counter starts at 0
   Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
+  Template.messages.helpers({
     counter: function () {
       return Session.get("counter");
+    },
+    messages: function () {
+        return Messages.find()
     }
   });
 
+  UI.registerHelper('getTraceData', function(){
+      // tracebuf messages are really in signed Int32. We are storing them in mongo 
+      // as a BinData field, which Meteor loads as a Uint8Array. The dtype field
+      // says what the datatype is; might deal with other types in the future.
+      if (this.dtype == "i4"){
+        traceDataView = new Int32Array(this.trace.buffer);
+        // Convert to normal array for display. Hopefully won't have to do this for plotting.
+        traceDataArray = Array.prototype.slice.call(traceDataView);
+        return traceDataArray;
+      };
+  });
+  
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
