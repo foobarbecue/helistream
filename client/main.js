c21bad2742ef03965892d13b24d61a414a60e3c9
Aaron Curtis <foobarbecue@gmail.com>
2014-10-27 12:41:43 -0600

    Restructure: divide into initial setup and update function.

--- client/main.js
+++ client/main.js
@@ -8,12 +8,13 @@ messages: function () {
 }
 });
 
-observeMsgs = Messages.find().observe({
-    added: function (document) {
+//observeMsgs = Messages.find().observe({
+//    added: function (document) {
+//        d3.selectAll("g .trLines").data()
 //         msgPlot.series[0].data.push(document);
-//         msgPlot.render();
-    }
-})
+////         msgPlot.render();
+//    }
+//})
     
 Template.msgPlot.destroyed = function(){
     if(observeMsgs) Observe.stop();
@@ -22,11 +23,35 @@ Template.msgPlot.destroyed = function(){
 Template.msgPlot.rendered = function() {
     Deps.autorun(function(){
     if (messageSub.ready()) {
-        plotMsgs(Messages.find({"chan": "BHZ"}).fetch());
+        var plot = d3.select("#plot")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(50,20)");
+
+        plot.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        plot.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Counts");
+
+        var subPlot = plot.append("g")
+            .attr("class", "trLines")
+
+        plotUpdate(subPlot, Messages.find({"chan": "BHZ"}).fetch());
     }})
 };
 
-getTraceData = function(msg){
+var getTraceData = function(msg){
     // tracebuf messages are really in signed Int32. We are storing them in mongo 
     // as a BinData field, which Meteor loads as a Uint8Array. The dtype field
     // says what the datatype is; might deal with other types in the future.
@@ -39,86 +64,62 @@ getTraceData = function(msg){
         };
 }
 
-getTraceTransform = function(msg){
-    pixelsPerMillisecond = ((xScale.range()[1] - xScale.range()[0]) / (xScale.domain()[1] - xScale.domain()[0]))
-    return "translate(" + xScale(msg.starttime) + ") scale(" +  1000 * pixelsPerMillisecond / msg.samprate + ", 1)"
-}
-
-plotMsgs = function(msgs){
+var getDataExtents = function(msgs){
     var starttime = d3.min(msgs, function(d) { return d.starttime; });
     var endtime = d3.max(msgs, function(d) { return d.endtime; });
     var minCounts = d3.min(msgs, function(msg) {
         return d3.min(getTraceData(msg));
-        });
+    });
     var maxCounts = d3.min(msgs, function(msg) {
         return d3.max(getTraceData(msg));
     });
+    return [startime, endtime, minCounts, maxCounts]
+}
 
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = 660 - margin.left - margin.right,
-        height = 200 - margin.top - margin.bottom;
+// plot setup
+var now = new Date();
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 660 - margin.left - margin.right,
+    height = 200 - margin.top - margin.bottom;
 
-    var xScale = d3.time.scale()
-        .domain([starttime, endtime])
-        .range([0, width]);
+var duration = 100000;
+var xScale = d3.time.scale()
+    .domain([now - duration, now])
+    .range([0, width]);
+var xScaleNoOffset = xScale.copy().domain([0,now - duration, now])
 
-    var xScaleNoOffset = xScale.copy().domain([0,endtime-starttime])
+var getTraceTransform = function(msg){
+    pixelsPerMillisecond = ((xScale.range()[1] - xScale.range()[0]) / (xScale.domain()[1] - xScale.domain()[0]))
+    return "translate(" + xScale(msg.starttime) + ") scale(" +  1000 * pixelsPerMillisecond / msg.samprate + ", 1)"
+}
 
-    var yScale = d3.scale.linear()
-        .range([height, 0]);
-        yScale.domain([minCounts, maxCounts]);
+var yScale = d3.scale.linear()
+    .domain([-100000, 100000])
+    .range([height, 0]);
 
-    var xAxis = d3.svg.axis()
-        .scale(xScale)
-        .orient("bottom");
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
 
-    var yAxis = d3.svg.axis()
-        .scale(yScale)
-        .orient("left");
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
 
-        //return d3.svg.line()
-        //    // d is the actual trace message
-        //    .x(function (d, i) {
-        //        return xScale(indToDateScale(i))
-        //    })
-        //    .y(function (d) {
-        //        return yScale(d);
-        //    });
-
-    d3svg = d3.select("#plot")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    d3svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    d3svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Counts");
-
-    var trLinesGrp = d3svg.append("g")
-        .attr("class", "trLines")
-        .datum(msgs)
+plotUpdate = function(tracePlot, msgs){
+    tracePlot.datum(msgs)
         .selectAll("g")
         .data(function(d){return(d)})
         .enter()
         .append("g")
-        .attr("class", "trace line")
+        .attr("class", "trace msg")
         .attr("transform",getTraceTransform)
         .append("path")
-        .datum(function(msg){return getTraceData(msg);})
+        .datum(
+            function(msg){return getTraceData(msg)},
+            function(msg){return msg._id})
 
-    trLinesGrp.attr("d",
+    tracePlot.selectAll("path")
+        .attr("d",
             d3.svg.line()
                 .x(function(d,i){return i;})
                 .y(function(d){return yScale(d);})
@@ -128,8 +129,8 @@ plotMsgs = function(msgs){
 
 UI.registerHelper('getTraceData', getTraceData);
 
-//TODO this should be in a .rendered
-msg = Messages.findOne({"chan": "BHZ"});
-if (!!msg){
-    plotTraceData(msg);
-}
+////TODO this should be in a .rendered
+//msgs = Messages.find({"chan": "BHZ"}).fetch();
+//if (!!msgs){
+//    plotUpdate(msgs);
+//}
