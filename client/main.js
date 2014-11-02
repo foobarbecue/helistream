1e3828827f7c25a96aca9860a51965a99c0803a7
Aaron Curtis <foobarbecue@gmail.com>
2014-11-02 10:48:40 -0700

    Zoom works now. Remove accidentally added axisexample.js .

--- client/main.js
+++ client/main.js
@@ -4,6 +4,7 @@ Messages = new Meteor.Collection("ew_msgs");
 var messageSub = Meteor.subscribe('ew_msgs', overall_starttime);
 var observeMsgs;
 var plotInitialized = false;
+var xOffset = 0;
 
 Template.messages.helpers({
 messages: function () {
@@ -24,7 +25,7 @@ Template.msgPlot.destroyed = function(){
 }
 
 // variables and supporting functions for plotting
-var now = new Date();
+var now = new Date().getTime();
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 660 - margin.left - margin.right,
     height = 200 - margin.top - margin.bottom;
@@ -32,7 +33,7 @@ var plotQuery = function(){
     return{'chan': 'BHZ', 'sta': 'CON', 'msgmod': /SCREAM2EW.*/}
 };
 var xScale = d3.time.scale()
-    .domain([now - Session.get('duration'), now])
+    .domain([new Date() - Session.get('duration'), new Date()])
     .range([0, width]);
 
 var xScaleNoOffset = xScale.copy().domain([0,now - Session.get('duration'), now])
@@ -48,7 +49,7 @@ var getTraceTransform = function(msg){
     yScaleParams = scaleToSlopeIntercept(yScale);
     pixelsPerMillisecond = xScaleParams['slope'];
     var sampRateXscale = 1000 * pixelsPerMillisecond / msg.samprate;
-    // TODO this is horrible, find an elegant way.
+    // TODO this is ugly, find an elegant way.
     return "translate("
         + xScale(msg.starttime)
         + "," + yScaleParams['intercept']
@@ -105,24 +106,39 @@ setAxesBounds = function(bounds){
 }
 
 plotUpdate = function(subPlot, msgs){
-    newExtents = getDataExtents(msgs);
-    newExtents['xMin'] = new Date() - Session.get('duration');
+    if (!!msgs) {
+        var newExtents = getDataExtents(msgs);
+    } else{
+        //TODO fix this it's bad
+        var newExtents = {xMin:xScale.domain()[0], xMax:xScale.domain()[1], yMin:yScale.domain()[0], yMax:yScale.domain()[1]}
+    }
+
+    newExtents['xMin'] = new Date().getTime() - Session.get('duration');
     setAxesBounds(newExtents);
 
     // Bind the new data and build child elements
-    subPlot.datum(msgs)
-        .selectAll("g")
-        .data(function(d){return(d)}, function(d){return(d._id)})
-        .enter()
-        .append("g")
-        .attr("class", "trace msg line")
-        .append("path")
-        .datum(
-        function(msg){return getTraceData(msg)},
-        function(msg){return msg._id});
-
+    if (!!msgs) {
+        subPlot.datum(msgs)
+            .selectAll("g")
+            .data(function (d) {
+                return (d)
+            }, function (d) {
+                return (d._id)
+            })
+            .enter()
+            .append("g")
+            .attr("class", "trace msg line")
+            .append("path")
+            .datum(
+            function (msg) {
+                return getTraceData(msg)
+            },
+            function (msg) {
+                return msg._id
+            });
+    }
     subPlot.selectAll("g")
-        .attr("transform",getTraceTransform)
+        .attr("transform",getTraceTransform);
 
     // Draw the lines
     subPlot.selectAll("path")
@@ -152,6 +168,12 @@ Deps.autorun(function(){
                 .attr("transform", "translate(50,20)")
                 .attr("class", "plotContainer");
 
+            // Put a white rectangle in the background so that plotContainer mouse events aren't triggered just on the data
+            plotContainer.append("rect")
+                .attr('width', width)
+                .attr('height', height)
+                .attr('fill', 'white');
+
             plotContainer.append("g")
                 .attr("class", "x axis")
                 .attr("transform", "translate(0," + height + ")")
@@ -167,19 +189,32 @@ Deps.autorun(function(){
                 .style("text-anchor", "end")
                 .text("Counts");
 
-            plotContainer.append("g")
+            var zoom = function() {
+                console.log(d3.event.scale)
+                Session.set('duration',Session.get('duration')*d3.event.scale)
+
+                //xOffset = d3.event.scale
+                //// Pass null for the messages parameter to avoid new query
+                //plotUpdate(subPlot, null, xOffset)
+            };
+
+            plotContainer
+                .call(d3.behavior.zoom().x(xScale).on("zoom", zoom))
+                .append("g")
                 .attr("class", "subplot");
+
+            var subPlot = d3.select("#plot .plotContainer .subplot")
+            subPlot.append("defs").append("svg:clipPath")
+                .attr("id","clip")
+                .append("rect")
+                .attr("width", 400)
+                .attr("height", 200);
 
             plotInitialized = true;
         }
 
-        var subPlot = d3.select("#plot .plotContainer .subplot")
-        subPlot.append("defs").append("svg:clipPath")
-            .attr("id","clip")
-            .append("rect")
-            .attr("width", 400)
-            .attr("height", 200);
 
+        var subPlot = d3.select("#plot .plotContainer .subplot")
         plotUpdate(subPlot, Messages.find(plotQuery()).fetch())
     }
 });
